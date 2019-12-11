553b782c13436c16e08b60480a6c7e55707df976
djendraa <djendraaa@gmail.com>
2019-12-11 15:25:44 +0700

    11 Des

--- cek_login.php
+++ cek_login.php
@@ -1,28 +1,27 @@
 <?php
 include "koneksi.php";
+
 session_start();
 $username = $_POST['username'];
 $password = $_POST['password'];
-if (empty($username)){
-echo "<script>alert('Username belum diisi')</script>";
-echo "<meta http-equiv='refresh' content='1 url=login.php'>";
-}else if (empty($password)){
-echo "<script>alert('Password belum diisi')</script>";
-echo "<meta http-equiv='refresh' content='1 url=login.php'>";
-}else{
 
-$login = mysqli_query($conn,"select * from muser where idUser='$username' and password='$password'");
-if (!$login) {
-    printf("Error: %s\n", mysqli_error($conn));
-    exit();
-}
-if (mysqli_num_rows($login) > 0){
+if (empty($username)){
+    echo "<script>alert('Username belum diisi')</script>";
+    echo "<meta http-equiv='refresh' content='1 url=login.php'>";
+} else if (empty($password)) {
+    echo "<script>alert('Password belum diisi')</script>";
+    echo "<meta http-equiv='refresh' content='1 url=login.php'>";
+} else {
+
+$login = mysqli_query($conn, "SELECT * FROM muser WHERE idUser='$username' and password='$password'");
+
+if (mysqli_num_rows($login) > 0) { 
 $_SESSION['username'] = $username;
-header("location:index.php");
-}else{
-echo "<script>alert('Username atau Password salah')</script>";
-echo "<meta http-equiv='refresh' content='1 url=login.php'>";
-}
+    header("location:index.php");
+} else {
+    echo "<script>alert('Username atau Password salah')</script>";
+    echo "<meta http-equiv='refresh' content='1 url=login.php'>";
+    }
 }
 
 ?>
