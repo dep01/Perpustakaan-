5420edd37e6e3c7f4cdeef977ef7e07df3f0347d
DEP <diazerlanggaputra@ymail.com>
2019-12-12 16:22:17 +0700

    peminjaman sukses

--- proses_peminjaman.php
+++ proses_peminjaman.php
@@ -8,13 +8,11 @@ header ("location:login.php");
 $nama = $_POST["nama_anggota"];
 $buku = $_POST["nama_buku"];
 $petugas = $_POST["nama_petugas"];
-$sql = "CALL spTrperpus('pinjam','$nama','0',$buku,$petugas)";
-
+//$sql = "CALL spTrperpus('pinjam','$nama','0',$buku,$petugas)"; coba dicek bedanya dimana
+$sql = "CALL spTrperpus('pinjam','$petugas','0',$buku,'$nama')";
 
 //call spTrperpus('kembali','NIK','$Nomor pinjam',0,0)
 // Buat query insert datanya
-
-
 // Jalankan query
 // isset digunakan untuk jika salah satu inputan tidak ter-input data tidak masuk ke database
 $hasil = mysqli_query($conn, $sql);
