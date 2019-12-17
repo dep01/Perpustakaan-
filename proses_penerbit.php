a76d6e647b410a4f92bc25d723e65466acfec64d
DEP <diazerlanggaputra@ymail.com>
2019-12-17 22:08:33 +0700

    tets

--- proses_penerbit.php
+++ proses_penerbit.php
@@ -2,39 +2,44 @@
 // File proses form
 include "koneksi.php";
 session_start();
-if (!isset($_SESSION['username'])){
-header ("location:login.php");
-}
-$penerbit = $_POST["penerbit"];
-$id = $_POST["id"];
-$proses = $_POST["proses"];
-if ($proses == "insert"){
-    $sql = "INSERT INTO mpenerbit(namaPenerbit,status) VALUES('$penerbit',1)";
-} else {
-    $sql = "UPDATE mpenerbit SET namaPenerbit = '$penerbit' WHERE idPenerbit = '$id'";
-}
+// if (!isset($_SESSION['username'])){
+// header ("location:login.php");
+// }
+// $penerbit = $_POST["penerbit"];
+// $id = $_POST["id"];
+// $proses = $_POST["proses"];
+// if ($proses == "insert"){
+//     $sql = "INSERT INTO mpenerbit(namaPenerbit,status) VALUES('$penerbit',1)";
+// } else {
+//     $sql = "UPDATE mpenerbit SET namaPenerbit = '$penerbit' WHERE idPenerbit = '$id'";
+// }
 
 // Buat query insert datanya
 
-
+if ($_POST['simpan']=="hapus"){
+    echo $_POST['simpan'];
+}else
+{
+    echo $_POST['simpan'];
+}
 // Jalankan query
 // isset digunakan untuk jika salah satu inputan tidak ter-input data tidak masuk ke database
-$hasil = mysqli_query($conn, $sql);
-if ($hasil){
-    echo "
-        <script>
-        alert('Data berhasil di tambahkan!');
-        document.location.href = 'index.php';
-        </script>
-        ";
+// $hasil = mysqli_query($conn, $sql);
+// if ($hasil){
+//     echo "
+//         <script>
+//         alert('Data berhasil di tambahkan!');
+//         document.location.href = 'index.php';
+//         </script>
+//         ";
         
-    } else {
-        echo "
-        <script>
-        alert('Data gagal di tambahkan!');
-        document.location.href = 'index.php';
-        </script>
-        ";
+//     } else {
+//         echo "
+//         <script>
+//         alert('Data gagal di tambahkan!');
+//         document.location.href = 'index.php';
+//         </script>
+//         ";
     
-    }
-?>
+//     }
+// ?>
